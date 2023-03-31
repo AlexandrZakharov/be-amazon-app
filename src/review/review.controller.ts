b98b7538c522c30d4af16c5fd9eff6c44bc2c681
alexandr <alex.zakharov2802@gmail.com>
2023-03-31 22:02:59 +0200

    Implemented review resource

--- src/review/review.controller.ts
+++ src/review/review.controller.ts
@@ -1,7 +1,36 @@
-import { Controller } from '@nestjs/common';
-import { ReviewService } from './review.service';
+import {
+  Body, Controller, Get, HttpCode, Param, Post, UsePipes, ValidationPipe
+} from "@nestjs/common";
+import { ReviewService } from "./review.service";
+import { CurrentUser } from "../auth/decorators/user.decorator";
+import { ReviewDto } from "./dto/review.dto";
+import { Auth } from "../auth/decorators/auth.decorator";
 
-@Controller('review')
+@Controller("reviews")
 export class ReviewController {
   constructor(private readonly reviewService: ReviewService) {}
+
+  @UsePipes(new ValidationPipe())
+  @Get()
+  async getAll() {
+    return this.reviewService.getAll();
+  }
+
+  @UsePipes(new ValidationPipe())
+  @HttpCode(200)
+  @Auth()
+  @Post("leave/:productId")
+  async create(
+    @CurrentUser("id") id: number,
+    @Param("productId") productId: string,
+    @Body() dto: ReviewDto
+  ) {
+    return this.reviewService.create(id, +productId, dto);
+  }
+
+  @UsePipes(new ValidationPipe())
+  @Get("average/:productId")
+  async getAverage(@Param("productId") productId: string) {
+    return this.reviewService.getAverageValueByProductId(+productId);
+  }
 }
--- src/review/review.service.ts
+++ src/review/review.service.ts
@@ -1,4 +1,55 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from "@nestjs/common";
+import { PrismaService } from "../prisma.service";
+import { CategoryDto } from "../category/dto/category.dto";
+import { faker } from "@faker-js/faker";
+import { returnReviewObject } from "./return-review.object";
+import { ReviewDto } from "./dto/review.dto";
 
 @Injectable()
-export class ReviewService {}
+export class ReviewService {
+  constructor(private prismaService: PrismaService) {}
+
+  async getAll() {
+    return this.prismaService.review.findMany({
+      orderBy: {
+        createdAt: "desc",
+      },
+      select: returnReviewObject,
+    });
+  }
+
+  async create(userId: number, productId: number, dto: ReviewDto) {
+    const product = await this.prismaService.product.findUnique({
+      where: { id: productId },
+    });
+
+    if (!product)
+      throw new NotFoundException(`Product with id ${productId} doesn't exist`);
+
+    return this.prismaService.review.create({
+      data: {
+        rating: dto.rating,
+        text: dto.text,
+        product: {
+          connect: {
+            id: productId,
+          },
+        },
+        user: {
+          connect: {
+            id: userId,
+          },
+        },
+      },
+    });
+  }
+
+  async getAverageValueByProductId(productId: number) {
+    return this.prismaService.review
+      .aggregate({
+        where: { productId },
+        _avg: { rating: true },
+      })
+      .then((data) => data._avg);
+  }
+}
