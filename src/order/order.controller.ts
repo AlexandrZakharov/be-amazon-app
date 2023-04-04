29105a550319751148255e2709e652941fcb3322
alexandr <alex.zakharov2802@gmail.com>
2023-04-04 14:04:29 +0200

    Implemented order resource

--- src/order/order.controller.ts
+++ src/order/order.controller.ts
@@ -1,7 +1,15 @@
-import { Controller } from '@nestjs/common';
-import { OrderService } from './order.service';
+import { Controller, Get, UsePipes, ValidationPipe } from "@nestjs/common";
+import { OrderService } from "./order.service";
+import { Auth } from "../auth/decorators/auth.decorator";
+import { CurrentUser } from "../auth/decorators/user.decorator";
 
-@Controller('order')
+@Controller("orders")
 export class OrderController {
   constructor(private readonly orderService: OrderService) {}
+
+  @Auth()
+  @Get()
+  async getAll(@CurrentUser("id") id: number) {
+    return this.orderService.getAll(id);
+  }
 }
